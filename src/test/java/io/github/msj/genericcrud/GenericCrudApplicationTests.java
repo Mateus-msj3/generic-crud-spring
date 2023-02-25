15c495fc738190f68c1dcf6f6beca2968da639f4
Mateus-msj3 <mateussouzaads@outlook.com>
2023-02-25 14:51:44 -0300

    setup inicial

---
+++ src/test/java/io/github/msj/genericcrud/GenericCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.msj.genericcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
